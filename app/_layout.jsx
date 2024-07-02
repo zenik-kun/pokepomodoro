79c994b0357c3e74c90bc1495aa358e18e4e02c9
zenik-kun <nikirukyun@gmail.com>
2024-07-02 12:28:24 +0530

    starting the project

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -36,7 +36,6 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{headerShown: false}} />
-      <Stack.Screen name='(auth)' options={{headerShown: false}} />
     </Stack>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -10,34 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className="h-full">
       <ScrollView contentContainerStyle={{ height:"100%" }}>
-        <View className="w-full justify-center items-center h-full px-5">
-          <View className = "flex-row justify-center items-center gap-1">
-            {/* <Image source={images.logoSmall} resizeMode='contain' className="w-[75px]"/> */}
-            <Text className="text-5xl text-pokered font-pokesolid py-5 pl-1">Poké</Text>
-            <Text className="text-5xl text-gray-100 font-pokehollow py-5 pr-1">Pomodoro</Text>
-          </View>
-
-          
-
-          <Image 
-            source={images.logoSmall}
-            className="max-w-[300px] w-full h-[200px]"
-            resizeMode='contain'
-          />
-
-          <Text className="text-4xl mt-4 tracking-wider py-8 px-2 text-black text-center font-pokehollow">
-              Focus and Catch 'em All!
-          </Text>
-          
-
-          <CustomButton 
-            title = "Continue with Email"
-            handlePress = {() => router.push('/sign-in')}
-            containerStyles="w-full"
-          />
-
-          <Link href="" className = "font-pokesolid p-4 text-xl tracking-widest">Skip</Link>
-        </View>
+        
       </ScrollView>
       <StatusBar backgroundColor='#F42C1C' style='light' />
     </SafeAreaView>
